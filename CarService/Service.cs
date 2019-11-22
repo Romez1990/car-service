38579e47f7d44063b93ada7409ab37ee01097392
Oleg <mr.oleg325@gmail.com>
2019-11-22 14:50:14 +0300

    Add Service class

---
+++ CarService/Service.cs
@@ -0,0 +1,16 @@
+﻿namespace CarService
+{
+    public class Service
+    {
+        public string UserId { get; set; }
+
+        public string Description { get; set; }
+
+        public float Cost { get; set; }
+
+        public override string ToString()
+        {
+            return $"{UserId} {Description} {Cost}";
+        }
+    }
+}
