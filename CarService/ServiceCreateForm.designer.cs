e612fd5cb1ac36572b4a79bcd28c4e91cfc015d0
Romez <Romez1990@gmail.com>
2019-12-19 21:23:36 +0300

    Change label text

--- CarService/ServiceCreateForm.designer.cs
+++ CarService/ServiceCreateForm.designer.cs
@@ -41,9 +41,9 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 20);
+            this.label1.Size = new System.Drawing.Size(156, 20);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Услуга:";
+            this.label1.Text = "Описание поломки:";
             // 
             // descriptionInfo
             // 
